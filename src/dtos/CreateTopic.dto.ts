7c6df8151c64e78ad22aa62b8ff77d1669e8f318
Jamile <milli.santana2017@gmail.com>
2026-06-30 09:52:16 -0300

    fix: adiciona mensagens de tratamento no dto

--- src/dtos/CreateTopic.dto.ts
+++ src/dtos/CreateTopic.dto.ts
@@ -2,25 +2,29 @@ import { IsNotEmpty, IsNumber, IsString } from "class-validator";
 
 export class CreateTopicDTO {
 	@IsString()
-	@IsNotEmpty()
+	@IsNotEmpty({ message: "Title is required" })
 	title!: string;
 
 	@IsString()
+	@IsNotEmpty({ message: "Description is required" })
 	description!: string;
 
 	@IsString()
+	@IsNotEmpty({ message: "Short description is required" })
 	shortDescription!: string;
 
 	@IsString()
 	references!: string;
 	
 	@IsString()
-	@IsNotEmpty()
+	@IsNotEmpty({ message: "Theme ID is required" })
 	themeId!: string;
 
 	@IsNumber()
+	@IsNotEmpty({ message: "Sequence is required" })
 	sequence!: number;
 
 	@IsString()
+	@IsNotEmpty({ message: "Video URL is required" })
 	videoUrl!: string;
 }
